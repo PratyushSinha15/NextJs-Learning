308324dc12c109161ea218d2ace7a62f746cd674
Pratyush Sinha <122526404+GitPratyush@users.noreply.github.com>
2024-08-21 02:22:44 +0530

    added database learnt how to use prisma in next also learn how to create backend route

--- next-backend/app/api/user/route.ts
+++ next-backend/app/api/user/route.ts
@@ -1,5 +1,10 @@
 //next route for user
 
+import { NextRequest } from "next/server"
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
 
 export function GET(){
     //logic
@@ -7,5 +12,25 @@ export function GET(){
     return Response.json({
         name:"Pratyush",
         email:"pratyushsinha@gmail.com"
+    })
+}
+
+export async function POST(req: NextRequest){
+   //extract the body 
+
+   const body= await req.json();
+
+   //store the body in database
+    await client.user.create({
+         data:{
+              username:body.name,
+              password:body.password
+            }
+    })
+   
+
+   //return the response
+    return Response.json({
+         message:"User created"
     })
 }
